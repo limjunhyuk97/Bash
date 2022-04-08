a567fcecf08bfdea00837b5a5c14ab15ed4bff23
jun_lim97 <limjunhyuk97@gmail.com>
2022-04-08 23:08:14 +0900

    Space for PA1

---
+++ __Shell__/MyShell/toy2.c
@@ -0,0 +1,18 @@
+//
+//  toy2.c
+//  PA1
+//
+//  Created by 임준혁 on 2022/04/08.
+//
+
+#include <stdio.h>
+
+int main(void){
+    
+    int a, b, c=1;
+    
+    a= b= c;
+    printf("\0");
+    
+    return 0;
+}
